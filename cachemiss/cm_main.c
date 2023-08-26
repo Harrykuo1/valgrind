c73773c77ad0ddd8d98a94e992e63bdc3b5cdef7
Harrykuo1 <stu970157@gmail.com>
2023-08-26 11:34:39 +0800

    Init new tool cachemiss

---
+++ cachemiss/cm_main.c
@@ -0,0 +1,73 @@
+
+/*--------------------------------------------------------------------*/
+/*--- Nulgrind: The minimal Valgrind tool.               cm_main.c ---*/
+/*--------------------------------------------------------------------*/
+
+/*
+   This file is part of Nulgrind, the minimal Valgrind tool,
+   which does no instrumentation or analysis.
+
+   Copyright (C) 2002-2017 Nicholas Nethercote
+      njn@valgrind.org
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+   The GNU General Public License is contained in the file COPYING.
+*/
+
+#include "pub_tool_basics.h"
+#include "pub_tool_tooliface.h"
+
+static void cm_post_clo_init(void)
+{
+}
+
+static
+IRSB* cm_instrument ( VgCallbackClosure* closure,
+                      IRSB* bb,
+                      const VexGuestLayout* layout, 
+                      const VexGuestExtents* vge,
+                      const VexArchInfo* archinfo_host,
+                      IRType gWordTy, IRType hWordTy )
+{
+   return bb;
+}
+
+static void cm_fini(Int exitcode)
+{
+}
+
+static void cm_pre_clo_init(void)
+{
+   VG_(details_name)            ("cachemiss");
+   VG_(details_version)         (NULL);
+   VG_(details_description)     ("the minimal Valgrind tool");
+   VG_(details_copyright_author)(
+      "Copyright (C) 2002-2017, and GNU GPL'd, by Nicholas Nethercote.");
+   VG_(details_bug_reports_to)  (VG_BUGS_TO);
+
+   VG_(details_avg_translation_sizeB) ( 275 );
+
+   VG_(basic_tool_funcs)        (cm_post_clo_init,
+                                 cm_instrument,
+                                 cm_fini);
+
+   /* No needs, no core events to track */
+}
+
+VG_DETERMINE_INTERFACE_VERSION(cm_pre_clo_init)
+
+/*--------------------------------------------------------------------*/
+/*--- end                                                          ---*/
+/*--------------------------------------------------------------------*/
